e03ecfb2a6acac9d1cd107204cf0721a57ebac4c
seboll13 <seboll13@gmail.com>
2020-02-22 14:42:43 +0100

    corrected test and bug

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, GreetingActivity.class);
-        EditText editText = (EditText) findViewById(R.id.mainName);
+        EditText editText = findViewById(R.id.mainName);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
